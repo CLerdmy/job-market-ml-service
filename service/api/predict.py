a55855c0bf658e5239a94a31240112820985ab99
CLerdmy <podturov@gmail.com>
2026-02-01 00:45:35 +0300

    add predict

---
+++ service/api/predict.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from service.schemas.request import JobFeatures
+from service.schemas.response import SalaryPrediction
+from service.services.predictor import predict_salary
+
+router = APIRouter()
+
+@router.post("/", response_model=SalaryPrediction)
+def predict(features: JobFeatures):
+    prediction = predict_salary(features.model_dump())
+    return SalaryPrediction(predicted_salary=prediction)
